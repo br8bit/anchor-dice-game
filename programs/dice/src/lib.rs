dfb997ccba1830559b67337b69e1f4bd3f2bdcc5
br8bit <>
2025-02-07 15:30:32 +0400

    add instructions

--- programs/dice/src/lib.rs
+++ programs/dice/src/lib.rs
@@ -2,15 +2,29 @@ use anchor_lang::prelude::*;
 
 declare_id!("8i4esFWjHMHWXhP3oH9WNzLTARtBmYXxuoWhpoy14sEU");
 
-pub mod instructions;
-pub mod state;
+mod errors;
+mod instructions;
+mod state;
+
+use instructions::*;
 
 #[program]
 pub mod dice {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        ctx.accounts.init(amount)
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, seed: u128, roll: u8, amount: u64) -> Result<()> {
+        ctx.accounts.place_bet(seed, roll, amount, &ctx.bumps)
+    }
+
+    pub fn resolve_bet(ctx: Context<ResolveBet>, sig: Vec<u8>) -> Result<()> {
+        ctx.accounts.resolve(&sig, &ctx.bumps)
+    }
+
+    pub fn refund_bet(ctx: Context<RefundBet>) -> Result<()> {
+        ctx.accounts.refund_bet(&ctx.bumps)
     }
 }
